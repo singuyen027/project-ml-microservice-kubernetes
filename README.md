01f5dcb5e1d8ea855d67956d7cd4f4cc57135eed
singuyen027 <singuyen027@gmail.com>
2022-08-10 18:17:03 +0700

    nguyen cmmit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<include a CircleCI status badge, here>
+[![singuyen027](https://circleci.com/gh/singuyen027/project-ml-microservice-kubernetes.svg?style=svg)](https://circleci.com/gh/singuyen027/project-ml-microservice-kubernetes.svg?style=svg)
 
 ## Project Overview
 
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,7 +9,7 @@ dockerpath="singuyen027/nguyenproject:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run nguyenproject --image=singuyen027/nguyenproject:v1.0.0 --port=80 --labels app=nguyenproject
+kubectl run nguyenproject --image=singuyen027/nguyenproject:v1.0.0 --port=8080
 
 # Step 3:
 # List kubernetes pods
@@ -17,4 +17,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward nguyenproject 8000:80
+kubectl port-forward nguyenproject 8080:80
